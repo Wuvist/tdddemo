455aa56421a4f79339e88e24be11f88e3c5e3caa
Weng Wei <wuvist@gmail.com>
2019-04-11 16:47:46 +0800

    Add test case

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/labstack/gommon v0.2.8 // indirect
 	github.com/mattn/go-colorable v0.1.1 // indirect
 	github.com/mattn/go-isatty v0.0.7 // indirect
+	github.com/stretchr/testify v1.3.0 // indirect
 	github.com/valyala/fasttemplate v1.0.1 // indirect
 	golang.org/x/crypto v0.0.0-20190404164418-38d8ce5564a5 // indirect
 )
--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"testing"
 )
 
@@ -18,15 +19,38 @@ func httpGet(url string) (result string, err error) {
 	return
 }
 
-func TestAbs(t *testing.T) {
+func httpPost(url string, data url.Values) (result string, err error) {
+	resp, err := http.PostForm(url, data)
+	if err != nil {
+		return
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	result = string(body)
+	return
+}
+
+func TestCounterAdd(t *testing.T) {
 	go main()
 
-	result, err := httpGet("http://localhost:8323/")
+	v := url.Values{}
+	v.Set("name", "Tom")
+	result, err := httpPost("http://localhost:8323/api/furyCount.Add", v)
 	if err != nil {
 		t.Error(err)
 	}
 
-	if result != "Hello, World!" {
-		t.Error("Invali body:" + result)
+	if result != "" {
+		t.Error("Add Fail:" + result)
+	}
+
+	result, err = httpPost("http://localhost:8323/api/furyCount.Get", v)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if result != "0" {
+		t.Error("Add Fail:" + result)
 	}
 }
